746e1e8bac4c6da13f47f5b8f651b0b7e2e5925b
Raymond Tan <raymond07065@gmail.com>
2024-03-04 21:26:37 +0800

    Time: 49 ms (47.17%) | Memory: 16.7 MB (45.44%) - LeetSync

---
+++ medium/12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,19 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        result  = ''
+        num_map = {
+            1000: "M", 900: "CM",
+            500: "D",  400: "CD",
+            100: "C",  90: "XC",
+            50: "L",   40: "XL",
+            10: "X",   9: "IX",
+            5: "V",    4: "IV",
+            1: "I"
+        }
+      
+        for key in num_map:
+            while num >= key:
+                result += num_map[key]
+                num -= key     
+
+        return result
